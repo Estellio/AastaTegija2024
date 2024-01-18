bd5f5cf169263928c1c091fb98ead7024309d11d
Estellio <pirko.vichmann@voco.ee>
2024-01-18 11:21:27 +0200

    Final demo

--- showAgenda.js
+++
@@ -1,5 +0,0 @@
-function show2ndDay() {
-    document.getElementById['schedule_d1'].style.display='none';
-    document.getElementById['schedule_d2'].style.display='block';
-    document.getElementById['schedule_d3'].style.display='none';
-}
